e4ccaafbe82fc387a4d038b8f06b1b69ebe57057
谢婉琪 <xiewanqi@wboll.com>
2016-02-01 11:12:25 +0800

    修改:解决文件上传时报请求体过长错误

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require("fs");
-var path = require("path");
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	path = require("path");
 
+
+//静态资 TODO:需要屏蔽服务端文件
+app.use(express.static(path.join(__dirname)));　
+app.use('/saveDoc', bodyParser({
+	keepExtensions: true,
+	limit: 10000000, // 10MB limit
+	defer: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
@@ -11,19 +18,22 @@ app.use(bodyParser.urlencoded({
 
 //添加文档
 app.post('/saveDoc', function(req, res) {
-	console.log();
 	console.log(req.body);
-
+	res.send('hello world');
 })
 
-//静态资 TODO:需要屏蔽服务端文件
-app.use(express.static(path.join(__dirname)));　
 
 //启动服务
-var port='8881';
-var server = app.listen(port, function() {
-	console.log('------------------------------------------');
-	console.log(' 点它:                           　　   ')
-	console.log(' http://%s:%s/views/index.html ', 'localhost', port)
-	console.log('------------------------------------------');
-});
+var server={
+	port:'8888',
+	domain:'localhost',
+	start:function(){
+		app.listen(server.port, function() {
+			console.log('------------------------------------------');
+			console.log(' 点它:                           　　   ')
+			console.log(' http://%s:%s/views/index.html ', server.domain, server.port)
+			console.log('------------------------------------------');
+		});
+	}
+}
+server.start();
