d32c6e4f34ea9a64075a33da54ad5c3b0c96e61b
谢婉琪 <xiewanqi@wboll.com>
2016-02-02 10:10:09 +0800

    修改：关联文件key

--- controller/save.js
+++
@@ -1,5 +0,0 @@
-var util = require('util'),
-	http = require('http'),
-	fs = require('fs'),
-	url = require('url'),
-	events = require('events');
--- doc/help.js
+++ doc/help.js
@@ -5,11 +5,11 @@ var help = [
 		children: [{
 			url: null,
 			description: '商户信息',
-			key: 'company-info'
+			key: 'companyinfo'
 		}, {
 			url: null,
 			description: '管理员信息',
-			key: 'company-adminInfo'
+			key: 'companyadminInfo'
 		}]
 	},
 	//微信管理
@@ -20,26 +20,26 @@ var help = [
 			children: [{
 				url: null,
 				description: '支付设置',
-				key: 'wechat-account'
+				key: 'wechataccount'
 			}]
 		}, {
 			description: '自定义设置',
 			children: [{
 				url: null,
 				description: '自定义菜单',
-				key: 'wechat-customSet-menuSet'
+				key: 'wechatcustomSetmenuSet'
 			}]
 		}, {
 			description: '微信设置',
 			children: [{
 				url: null,
 				description: '微信授权',
-				key: 'wechat-wechatSet-authorization'
+				key: 'wechatwechatSetauthorization'
 			}]
 		}, {
 			url: null,
 			description: '自动回复',
-			key: 'wechat-autoReply'
+			key: 'wechatautoReply'
 		}]
 	},
 
@@ -51,7 +51,7 @@ var help = [
 			{
 				url: null,
 				description: '商城概况',
-				key: 'mall-mallSurvey'
+				key: 'mallmallSurvey'
 			},
 			//店铺管理
 			{
@@ -59,7 +59,7 @@ var help = [
 				children: [{
 					url: null,
 					description: '店铺主页',
-					key: 'mall-store-customPage'
+					key: 'mallstorecustomPage'
 				}]
 			},
 			//物流管理
@@ -68,11 +68,11 @@ var help = [
 				children: [{
 					url: null,
 					description: '运费模板',
-					key: 'mall-logistics-carriageModule'
+					key: 'malllogisticscarriageModule'
 				}, {
 					url: null,
 					description: '自提管理',
-					key: 'mall-logistics-selfExtracting'
+					key: 'malllogisticsselfExtracting'
 				}]
 			},
 			//商品管理
@@ -81,40 +81,40 @@ var help = [
 				children: [{
 					url: null,
 					description: '商品列表',
-					key: 'mall-commodity-list'
+					key: 'mallcommoditylist'
 				}, {
 					url: null,
 					description: '商品基本信息',
-					key: 'mall-commodity-createBaseInfo'
+					key: 'mallcommoditycreateBaseInfo'
 				}, {
 					url: null,
 					description: '商品详情',
-					key: 'mall-commodity-createDesc'
+					key: 'mallcommoditycreateDesc'
 				}, {
 					url: null,
 					description: '商品营销信息',
-					key: 'mall-commodity-createMarketingInfo'
+					key: 'mallcommoditycreateMarketingInfo'
 				}, {
 					url: null,
 					description: '商品分组列表',
-					key: 'mall-commodity-grouping'
+					key: 'mallcommoditygrouping'
 
 				}, {
 					url: null,
 					description: '创建商品分组基本信息',
-					key: 'mall-commodity-groupCreateBaseInfo'
+					key: 'mallcommoditygroupCreateBaseInfo'
 				}, {
 					url: null,
 					description: '关联商品分组商品',
-					key: 'mall-commodity-groupCreatechoseCommodity'
+					key: 'mallcommoditygroupCreatechoseCommodity'
 				}, {
 					url: null,
 					description: '编辑商品分组基本信息',
-					key: 'mall-commodity-groupEditBaseInfo'
+					key: 'mallcommoditygroupEditBaseInfo'
 				}, {
 					url: null,
 					description: '修改关联商品分组商品',
-					key: 'mall-commodity-groupEditchoseCommodity'
+					key: 'mallcommoditygroupEditchoseCommodity'
 				}]
 			},
 			//订单管理
@@ -123,11 +123,11 @@ var help = [
 				children: [{
 					url: null,
 					description: '订单列表',
-					key: 'mall-order-list'
+					key: 'mallorderlist'
 				}, {
 					url: null,
 					description: '订单详情',
-					key: 'mall-order-detail'
+					key: 'mallorderdetail'
 				}]
 			},
 			//售后管理
@@ -136,11 +136,11 @@ var help = [
 				children: [{
 					url: null,
 					description: '退换货管理',
-					key: 'mall-afterService-exchange'
+					key: 'mallafterServiceexchange'
 				}, {
 					url: null,
 					description: '商品评价',
-					key: 'mall-afterService-evaluation'
+					key: 'mallafterServiceevaluation'
 				}]
 			},
 			//会员管理
@@ -149,19 +149,19 @@ var help = [
 				children: [{
 					url: null,
 					description: '会员列表',
-					key: 'mall-afterService-list'
+					key: 'mallafterServicelist'
 				}, {
 					url: null,
 					description: '会员等级与权益',
-					key: 'mall-afterService-rights'
+					key: 'mallafterServicerights'
 				}, {
 					url: null,
 					description: '会员详情',
-					key: 'mall-afterService-detail'
+					key: 'mallafterServicedetail'
 				}, {
 					url: null,
 					description: '会员等级设置',
-					key: 'mall-afterService-levelSetting'
+					key: 'mallafterServicelevelSetting'
 				}]
 			},
 			//分销管理
@@ -170,39 +170,39 @@ var help = [
 				children: [{
 					url: null,
 					description: '分销申请设置',
-					key: 'mall-distribution-applySet'
+					key: 'malldistributionapplySet'
 				}, {
 					url: null,
 					description: '分销商自动审核设置',
-					key: 'mall-distribution-automaticVerify'
+					key: 'malldistributionautomaticVerify'
 				}, {
 					url: null,
 					description: '分销申请审核',
-					key: 'mall-distribution-applyVerifyList'
+					key: 'malldistributionapplyVerifyList'
 				}, {
 					url: null,
 					description: '分销商品',
-					key: 'mall-distribution-commodityList'
+					key: 'malldistributioncommodityList'
 				}, {
 					url: null,
 					description: '分销商列表',
-					key: 'mall-distribution-list'
+					key: 'malldistributionlist'
 				}, {
 					url: null,
 					description: '分销商详情',
-					key: 'mall-distribution-detail'
+					key: 'malldistributiondetail'
 				}, {
 					url: null,
 					description: '分销等级设置',
-					key: 'mall-distribution-level'
+					key: 'malldistributionlevel'
 				}, {
 					url: null,
 					description: '佣金提现设置',
-					key: 'mall-distribution-commissionCashSet'
+					key: 'malldistributioncommissionCashSet'
 				}, {
 					url: null,
 					description: '分销商政策',
-					key: 'mall-distribution-policy'
+					key: 'malldistributionpolicy'
 				}]
 			},
 			//财务管理
@@ -211,27 +211,27 @@ var help = [
 				children: [{
 					url: null,
 					description: '商城收入记录',
-					key: 'mall-finance-incomeRecord'
+					key: 'mallfinanceincomeRecord'
 				}, {
 					url: null,
 					description: '分销佣金记录',
-					key: 'mall-finance-commissionRecord'
+					key: 'mallfinancecommissionRecord'
 				}, {
 					url: null,
 					description: '佣金提现申请',
-					key: 'mall-finance-withdrawCashApply'
+					key: 'mallfinancewithdrawCashApply'
 				}, {
 					url: null,
 					description: '提现批准记录',
-					key: 'mall-finance-withdrawCashPass'
+					key: 'mallfinancewithdrawCashPass'
 				}, {
 					url: null,
 					description: '提现驳回记录',
-					key: 'mall-finance-withdrawCashReject'
+					key: 'mallfinancewithdrawCashReject'
 				}, {
 					url: null,
 					description: '佣金转余额记录',
-					key: 'mall-finance-commissionToBalance'
+					key: 'mallfinancecommissionToBalance'
 				}]
 			}
 			//
@@ -246,11 +246,11 @@ var help = [
 			children: [{
 				url: null,
 				description: '订单及收货设置',
-				key: 'systemSet-mallSet-orderSet'
+				key: 'systemSetmallSetorderSet'
 			}, {
 				url: null,
 				description: '系统功能名称',
-				key: 'systemSet-mallSet-moduleNameSet'
+				key: 'systemSetmallSetmoduleNameSet'
 			}]
 		}]
 	},
@@ -263,15 +263,15 @@ var help = [
 			children: [{
 				url: null,
 				description: '优惠券列表',
-				key: 'marketing-coupon-list'
+				key: 'marketingcouponlist'
 			}, {
 				url: null,
 				description: '优惠券设置',
-				key: 'marketing-coupon-couponSet'
+				key: 'marketingcouponcouponSet'
 			}, {
 				url: null,
 				description: '优惠券投放设置',
-				key: 'marketing-coupon-couponSendSet'
+				key: 'marketingcouponcouponSendSet'
 			}]
 		}]
 	}
